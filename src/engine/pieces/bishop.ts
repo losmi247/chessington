5a2cd77d1389996042ce1a35d61ef1a87bf7d7e1
milospuric <milos.puric@softwire.com>
2024-06-26 17:15:30 +0100

    Added bishop movement

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,17 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let currentSquare = board.findPiece(this);
+        let availableMoves = new Array(0);
+
+        for (let i = 1; i < 8; i++) {
+            availableMoves.push(new Square(currentSquare.row - i, currentSquare.col - i));
+            availableMoves.push(new Square(currentSquare.row + i, currentSquare.col - i));
+            availableMoves.push(new Square(currentSquare.row - i, currentSquare.col + i));
+            availableMoves.push(new Square(currentSquare.row + i, currentSquare.col + i));
+        }
+
+        // Remove out-of-bounds squares
+        return availableMoves.filter((s: Square) => s.col >= 0 && s.col <= 7 && s.row >= 0 && s.row <= 7);
     }
 }
