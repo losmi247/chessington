5a19444ce9b8ae25f71af6b5fe91d5f1c3a169d0
milospuric <milos.puric@softwire.com>
2024-06-26 16:37:46 +0100

    Added initial rook movement

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,20 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let square = board.findPiece(this);
+        let moves = new Array(0);
+
+        for (let i = 0; i < 8; i++) {
+            //Horizontal
+            if (i !== square.col) {
+                moves.push(new Square(square.row, i));
+            }
+            //Vertical
+            if (i !== square.row) {
+                moves.push(new Square(i, square.col));
+            }
+        }
+
+        return moves;
     }
 }
