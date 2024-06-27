4566460be8bf4dc733ac55189cdd8bdc0b2f2dd1
milospuric <milos.puric@softwire.com>
2024-06-27 11:55:37 +0100

    implement king movement

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,19 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let currentSquare = board.findPiece(this);
+        let availableMoves = [
+            new Square(currentSquare.row + 1, currentSquare.col),
+            new Square(currentSquare.row - 1, currentSquare.col),
+            new Square(currentSquare.row, currentSquare.col + 1),
+            new Square(currentSquare.row, currentSquare.col - 1),
+            new Square(currentSquare.row + 1, currentSquare.col + 1),
+            new Square(currentSquare.row - 1, currentSquare.col - 1),
+            new Square(currentSquare.row - 1, currentSquare.col + 1),
+            new Square(currentSquare.row + 1, currentSquare.col - 1)
+        ];
+
+        // Remove out-of-bounds squares
+        return availableMoves.filter((s: Square) => s.col >= 0 && s.col <= 7 && s.row >= 0 && s.row <= 7);
     }
 }
