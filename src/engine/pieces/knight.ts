7a2f23018462c1b94121a01947206b954a097591
milospuric <milos.puric@softwire.com>
2024-06-27 11:17:22 +0100

    implement knight movement

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,18 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let currentSquare = board.findPiece(this);
+        let availableMoves = new Array(0);
+
+        for(let horizontalDirection of [-1,1]){
+            for(let verticalDirection of [-1,1]){
+                availableMoves.push(
+                    new Square(currentSquare.row + horizontalDirection, currentSquare.col + verticalDirection * 2),
+                    new Square(currentSquare.row + horizontalDirection * 2, currentSquare.col + verticalDirection),
+                );
+            }
+        }
+
+        return availableMoves.filter((s: Square) => s.col >= 0 && s.col <= 7 && s.row >= 0 && s.row <= 7);
     }
 }
